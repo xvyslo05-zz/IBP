23a1043483dafe953f714abb52b52dad5eea3e68
rvyslouzil <xvyslo05@stud.fit.vutbr.cz>
2019-03-16 17:09:30 +0100

    add pretty print function

--- model2/model2.py
+++ model2/model2.py
@@ -5,12 +5,19 @@ def model1(y, t):
     x = y
     dydt = -x
     err = x - numpy.sin(t)
-    print(err)
-    print(dydt)
+    giveResults(err, x, y, t)
     return dydt
+
+def giveResults(err, x, y, t):
+    print("===========")
+    print("err:", err)
+    print("x:", x)
+    print("y:", y)
+    print("t:", t)
 
 y0 = 1
 
 t = numpy.linspace(0, 20)
 
 y = odeint(model1, y0, t)
+
