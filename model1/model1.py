4095678f094252033a8ba79f7bac155922349416
rvyslouzil <xvyslo05@stud.fit.vutbr.cz>
2019-05-02 13:30:26 +0200

    random shit

--- model1/model1.py
+++ model1/model1.py
@@ -4,12 +4,15 @@
 import math
 import random
 import pprint
+import matplotlib.pyplot as plt
 
 height      = 2     # in meters
 velocity    = 10    # in m/s
 g           = 9.8   # acceleration in m/s^2
 cr          = 0.9   # coeficient of restitution, from 0 to 1, where 1 equals perfect elastic collision
 
+velocity_array = [velocity]
+height_array = [height]
 
 
 pprint.pprint(['#', 'velocity', 'restitution_velocity', 'height'])
@@ -22,8 +25,21 @@ for j in range(20):
 
     result = [j, velocity, restitution_velocity, height]
 
+    velocity_array.append(velocity)
+    height_array.append(height)
+
     pprint.pprint(result)
 
     if height < 0.0000000001 :
       print("END OF SIMULATION")
       break
+
+print(velocity_array)
+print(height_array)
+
+plt.plot(height_array, velocity_array)
+
+plt.ylabel('rychlost')
+plt.xlabel('vyska')
+
+plt.show()
