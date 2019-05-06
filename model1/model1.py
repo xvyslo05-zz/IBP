46a9f89644f97d0bfc0b4f49e2f7b109738fa371
rvyslouzil <xvyslo05@stud.fit.vutbr.cz>
2019-05-06 12:55:53 +0200

    bouncing ball python change, not bouncing

--- model1/model1.py
+++ model1/model1.py
@@ -1,44 +1,48 @@
 from scipy.misc import derivative
 from scipy.integrate import odeint
+from scipy.integrate import solve_ivp
 import matplotlib.pyplot as pyplot
 import numpy as np
 import time
 
 start_time = time.time()
 
-position = 10
+position = 0.1
 velocity = 15
 cr = -0.8
+force = 10000
 
-down = True
-
-def bounce(y, t, down):
-
-    # print(y)
+def bounce(y, t):
 
     dydt0 = y[1]
     dydt1 = -9.81
 
-    if(y[0] <= 2 and down):
-      dydt0 = -dydt0
-      dydt1 = -dydt1 * cr
-      down = False
+
+    if(y[0] <= 0):
+      dydt1 += force / (1 + np.exp(y[0]/0.001))
 
 
-    dydt = [dydt0, dydt1, down]
+
+
+    dydt = [dydt0, -dydt1*cr]
     return dydt
 
 
-t = np.linspace(0, 10)
+t = np.linspace(0, 100, 1000)
 
 y0 = [position, velocity]
 
-z = odeint(bounce, y0, t, args=(down,))
+z = odeint(bounce, y0, t, hmax=0.01)
 
-print(position)
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
-pyplot.plot(t, z[:,0])
+pyplot.plot(t, z[:, 0], 'r', label='position(t)')
 
+
+pyplot.grid()
+pyplot.legend(loc='best')
 pyplot.show()
+
+
+
