f07fd8e8978b9e3dbd98a7545b742b304b2fe40a
rvyslouzil <xvyslo05@stud.fit.vutbr.cz>
2019-05-06 12:55:27 +0200

    kruhovy test matlab  done

--- model2/model2.m
+++ model2/model2.m
@@ -1,1 +1,16 @@
-% @TODO: model for matlab
+syms u(t) v(t)
+
+ode1 = diff(u) == v;
+ode2 = diff(v) == -u;
+
+odes=[ode1; ode2]
+
+S = dsolve(odes)
+
+uSol(t) = S.u
+vSol(t) = S.v
+
+cond1 = u(0) == 0;
+cond2 = v(0) == 1;
+conds = [cond1; cond2];
+[uSol(t), vSol(t)] = dsolve(odes,conds)
