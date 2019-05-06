260986a1b23e5e0178062dc5f444dcd1cbd09af2
rvyslouzil <xvyslo05@stud.fit.vutbr.cz>
2019-05-06 23:50:50 +0200

    add stiff model first try

--- model3/model3.py
+++ model3/model3.py
@@ -2,8 +2,26 @@
 
 # stiff system
 
+from scipy.integrate import odeint
+import matplotlib.pyplot as pyplot
+import numpy as np
+import math
+import time
 
-dy = -50(y - math.cos(x)) # main equation
+def stiff(y, t):
+  dy = -50*(y - math.cos(x))
+  return dy
 
+# dy = -50(y - math.cos(x)) # main equation
 
-(yn - y)/h = f(xn, yn) # pro h=1.974/50 a h=1.875/50
+t = np.linspace(0, 1, 5)
+
+y0 = 1
+
+solve = odeint(stiff, y0, t, hmax=1.974/50)
+
+pyplot.plot(solve, t)
+
+pyplot.show()
+
+# (yn - y)/h = f(xn, yn) # pro h=1.974/50 a h=1.875/50
