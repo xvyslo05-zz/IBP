3d3faece232bd1e1d0c9465f756840d8effe6819
rvyslouzil <xvyslo05@stud.fit.vutbr.cz>
2019-05-08 21:51:55 +0200

    25 NS confirmed

--- model1/test.py
+++ model1/test.py
@@ -2,27 +2,34 @@ from scipy.integrate import solve_ivp
 import matplotlib.pyplot as pyplot
 
 def bounce(t, y):
+    if(y[0] <= 0):
+      dydt1 = -y[1] * -0.8
+    else:
+      dydt1 = -9.81
+
     dydt0 = y[1]
-    dydt1 = -9.81
-    print('asdasdas')
+
     dydt = [dydt0, dydt1]
+    print(y[0])
+    print(dydt)
     return dydt
 
 def situation(t, y):
     dydt1 = -y[1] * -0.8
-    print('picovina')
 
+    # print('bounce')
+    # print(dydt1)
     return dydt1
 
 situation.terminal = True
 situation.direction = 1
 
-count = solve_ivp(bounce, [0, 100], [1, 15], events=situation)
+count = solve_ivp(bounce, [0, 6], [10, 15], events=situation)
 
 print(count.t_events)
 print(count.t)
 print(count.y)
 
-pyplot.plot(count.t)
+pyplot.plot(count.y)
 pyplot.show()
 
--- model3/model3.py
+++ model3/model3.py
@@ -4,24 +4,42 @@
 
 from scipy.integrate import odeint
 import matplotlib.pyplot as pyplot
-import numpy as np
+# import numpy as np
 import math
-import time
+# import time
 
-def stiff(y, t):
-  dy = -50*(y - math.cos(x))
-  return dy
+# def stiff(y, t):
+#   dy = -50*(y - math.cos(x))
+#   return dy
 
-# dy = -50(y - math.cos(x)) # main equation
+# # dy = -50(y - math.cos(x)) # main equation
 
-t = np.linspace(0, 1, 5)
+# t = np.linspace(0, 1, 5)
 
-y0 = 1
+# y0 = 1
 
-solve = odeint(stiff, y0, t, hmax=1.974/50)
+# solve = odeint(stiff, y0, t, hmax=1.974/50)
 
-pyplot.plot(solve, t)
+# pyplot.plot(solve, t)
 
+# pyplot.show()
+
+# # (yn - y)/h = f(xn, yn) # pro h=1.974/50 a h=1.875/50
+
+from scipy.integrate import ode
+
+y0, t0 = 0, 0
+
+def f(t, y, arg1):
+    return -50*(y - math.cos(t))
+
+
+r = ode(f).set_integrator('zvode', method='bdf', min_step=1.974/50, max_step=1.974/50)
+r.set_initial_value(y0, t0).set_f_params(2.0)
+t1 = 1000
+dt = 1
+while r.successful() and r.t < t1:
+    print(r.t+dt, r.integrate(r.t+dt))
+
+pyplot.plot(r.t)
 pyplot.show()
-
-# (yn - y)/h = f(xn, yn) # pro h=1.974/50 a h=1.875/50
