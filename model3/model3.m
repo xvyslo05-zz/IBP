81c72c512aa2c56cc8a832287b746a2b8858e6d6
rvyslouzil <xvyslo05@stud.fit.vutbr.cz>
2019-05-10 15:10:25 +0200

    stiff models done

--- model3/model3.m
+++ model3/model3.m
@@ -1,1 +1,13 @@
-// @TODO:  model 3 MATLAB
+inittime = clock;
+
+tspan = [0 2];
+
+y0 = 1;
+
+[t,y] = ode15s(@(t,y) -10*(y - cos(t)), tspan, y0);
+
+plot(t, y)
+legend('y', 'Location', 'best')
+
+endtime = clock - inittime;
+disp(endtime);
--- model3/model3.mo
+++ model3/model3.mo
@@ -1,1 +1,8 @@
 // @TODO: model 3 for modelica
+
+model stiff_system
+  Real x;
+  Real y (start = 1);
+equation
+  der(y) = -50*(y - cos(x))
+end stiff_system;
